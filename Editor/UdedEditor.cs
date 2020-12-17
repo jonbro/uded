d2a1556283c9a99310a6c5dc0c66a232231c10c8
Jonathan Brodsky <jonathan.brodsky@unity3d.com>
2020-12-16 16:45:58 -0800

    Added menu item to create a new uded

--- Editor/UdedEditor.cs
+++ Editor/UdedEditor.cs
@@ -7,31 +7,35 @@ using UnityEngine.Rendering.VirtualTexturing;
 
 namespace Uded
 {
-    [CustomEditor( typeof( UdedCore ) )]
+    [CustomEditor(typeof(UdedCore))]
     public class UdedEditor : Editor
     {
         private int currentFace = -1;
         private int debugVert = -1;
         private int fixLink = -1;
+
         public override void OnInspectorGUI()
         {
-            var uded = (UdedCore)target;
-            DrawDefaultInspector ();
+            var uded = (UdedCore) target;
+            DrawDefaultInspector();
             if (GUILayout.Button("Rebuild"))
             {
                 uded.Rebuild();
                 EditorUtility.SetDirty(uded);
             }
+
             if (GUILayout.Button("Clear"))
             {
                 uded.Clear();
                 EditorUtility.SetDirty(uded);
             }
+
             debugVert = EditorGUILayout.IntField(debugVert);
             if (GUILayout.Button("DebugVert") && debugVert > 0 && debugVert < uded.Vertexes.Count)
             {
                 uded.DebugExitsForVert(debugVert);
             }
+
             fixLink = EditorGUILayout.IntField(fixLink);
             if (GUILayout.Button("Fix Link") && fixLink > 0 && fixLink < uded.Edges.Count)
             {
@@ -43,7 +47,7 @@ namespace Uded
         {
             // get the chosen game object
             var uded = target as UdedCore;
-            
+
             if (!uded.displayDebug)
                 return;
             HashSet<HalfEdge> displayedEdges = new HashSet<HalfEdge>();
@@ -52,19 +56,22 @@ namespace Uded
             for (int i = 0; i < uded.Edges.Count; i++)
             {
                 var edge = uded.Edges[i];
-                if(displayedEdges.Contains(edge))
+                if (displayedEdges.Contains(edge))
                     continue;
                 Handles.color = Color.black;
                 // offset to the left
-                var forward = (Vector3)uded.EdgeVertex(uded.GetTwin(i)) - (Vector3)uded.EdgeVertex(edge);
+                var forward = (Vector3) uded.EdgeVertex(uded.GetTwin(i)) - (Vector3) uded.EdgeVertex(edge);
                 var forwardRot = Quaternion.LookRotation(forward);
                 var arrowRot = forwardRot * Quaternion.AngleAxis(190, Vector3.up);
                 Vector3 left = forwardRot * Vector3.left * 0.02f;
-                Handles.DrawLine((Vector3)(Vector3)uded.EdgeVertex(edge)+left,(Vector3)uded.EdgeVertex(uded.GetTwin(i))+left);
-                var center = Vector3.Lerp((Vector3) (Vector3)uded.EdgeVertex(edge)+left, (Vector3)uded.EdgeVertex(uded.GetTwin(i))+left, 0.5f);
+                Handles.DrawLine((Vector3) (Vector3) uded.EdgeVertex(edge) + left,
+                    (Vector3) uded.EdgeVertex(uded.GetTwin(i)) + left);
+                var center = Vector3.Lerp((Vector3) (Vector3) uded.EdgeVertex(edge) + left,
+                    (Vector3) uded.EdgeVertex(uded.GetTwin(i)) + left, 0.5f);
                 // arrow displaying orientation
-                Handles.DrawLine((Vector3)uded.EdgeVertex(uded.GetTwin(i))+left, (Vector3)uded.EdgeVertex(uded.GetTwin(i))+left+arrowRot*Vector3.forward*0.1f);
-                Handles.Label(center, ""+count++);
+                Handles.DrawLine((Vector3) uded.EdgeVertex(uded.GetTwin(i)) + left,
+                    (Vector3) uded.EdgeVertex(uded.GetTwin(i)) + left + arrowRot * Vector3.forward * 0.1f);
+                Handles.Label(center, "" + count++);
                 displayedEdges.Add(edge);
             }
 
@@ -75,14 +82,25 @@ namespace Uded
                 foreach (var vertex in uded.Vertexes)
                 {
                     Handles.color = Color.white;
-                    Handles.DrawSolidDisc((Vector3)vertex, Vector3.up, 0.01f);
+                    Handles.DrawSolidDisc((Vector3) vertex, Vector3.up, 0.01f);
                     Handles.color = Color.black;
-                    Handles.DrawWireDisc((Vector3)vertex, Vector3.up, 0.01f);
-                    Handles.Label((Vector3)vertex, ""+count++);
+                    Handles.DrawWireDisc((Vector3) vertex, Vector3.up, 0.01f);
+                    Handles.Label((Vector3) vertex, "" + count++);
                 }
-                Handles.Label((Vector3)uded.Vertexes[0], "edges: " + uded.Edges.Count);
-            }    
+
+                Handles.Label((Vector3) uded.Vertexes[0], "edges: " + uded.Edges.Count);
+            }
+        }
+
+        [MenuItem("GameObject/Uded", false, 10)]
+        public static void CreateNewUded(MenuCommand menuCommand)
+        {
+            var go = new GameObject("Uded");
+            go.AddComponent<UdedCore>();
+            GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+            // Register the creation in the undo system
+            Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+            Selection.activeObject = go;
         }
     }
-    
 }
